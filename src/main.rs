39ecad1a222edad22f0e33013d8447db1812439c
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2025-02-14 13:03:49 +0300

    parsing json

--- src/main.rs
+++ src/main.rs
@@ -12,50 +12,72 @@ fn get_pci_profiles_from_url() {
     let res: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
     if let serde_json::Value::Array(profiles) = &res["profiles"] {
         for profile in profiles {
-            let codename = &profile["codename"].as_str();
-            /*let driver_name = driver["driver"].as_str().to_owned().unwrap().to_string();
-            let driver_device = result.unwrap();
-            let driver_icon = driver["icon"].as_str().to_owned().unwrap().to_string();
-            let driver_experimental = driver["experimental"].as_bool().unwrap();
-            let driver_removeble = driver["removable"].as_bool().unwrap();
-            let command_version_label =
-                Command::new("/usr/lib/pika/drivers/generate_package_info.sh")
-                    .args(["version", &driver_name])
-                    .output()
-                    .unwrap();
-            let command_description_label =
-                Command::new("/usr/lib/pika/drivers/generate_package_info.sh")
-                    .args(["description", &driver_name])
-                    .output()
-                    .unwrap();
-            let found_driver_package = DriverPackage {
-                driver: driver_name,
-                version: String::from_utf8(command_version_label.stdout)
-                    .unwrap()
-                    .trim()
-                    .to_string(),
-                device: driver_device,
-                description: String::from_utf8(command_description_label.stdout)
-                    .unwrap()
-                    .trim()
-                    .to_string(),
-                icon: driver_icon,
-                experimental: driver_experimental,
-                removeble: driver_removeble,
-            };*/
+            let codename = profile["codename"].as_str().unwrap_or_default().to_string();
+            let icon_name = profile["icon_name"].to_string();
+            let class_ids: Vec<String> = profile["class_ids"].as_array().expect("invalid_profile_class_ids").into_iter().map(|x| x.as_str().unwrap_or_default().to_string()).collect();
+            let vendor_ids: Vec<String> = profile["vendor_ids"].as_array().expect("invalid_profile_class_ids").into_iter().map(|x| x.as_str().unwrap_or_default().to_string()).collect();
+            let device_ids: Vec<String> = profile["device_ids"].as_array().expect("invalid_profile_class_ids").into_iter().map(|x| x.as_str().unwrap_or_default().to_string()).collect();
+            let blacklisted_class_ids: Vec<String> = profile["blacklisted_class_ids"].as_array().expect("invalid_profile_class_ids").into_iter().map(|x| x.as_str().unwrap_or_default().to_string()).collect();
+            let blacklisted_vendor_ids: Vec<String> = profile["blacklisted_vendor_ids"].as_array().expect("invalid_profile_class_ids").into_iter().map(|x| x.as_str().unwrap_or_default().to_string()).collect();
+            let blacklisted_device_ids: Vec<String> = profile["blacklisted_device_ids"].as_array().expect("invalid_profile_class_ids").into_iter().map(|x| x.as_str().unwrap_or_default().to_string()).collect();
+            let packages: Option<Vec<String>> = match profile["packages"].as_str() {
+                Some(_) => None,
+                None => Some(profile["packages"].as_array().expect("invalid_profile_class_ids").into_iter().map(|x| x.as_str().unwrap_or_default().to_string()).collect())
+            };
+            let install_script_value = profile["install_script"].as_str().unwrap_or_default().to_string();
+            let install_script = match install_script_value.as_str()  {
+                "Option::is_none" => {
+                    None
+                }
+                _ => {
+                    Some(install_script_value)
+                }
+            };
+            let remove_script_value = profile["remove_script"].as_str().unwrap_or_default().to_string();
+            let remove_script = match remove_script_value.as_str()  {
+                "Option::is_none" => {
+                    None
+                }
+                _ => {
+                    Some(remove_script_value)
+                }
+            };
+            let experimental= profile["experimental"].as_bool().unwrap_or_default();
+            let removable= profile["removable"].as_bool().unwrap_or_default();
+            let priority= profile["priority"].as_i64().unwrap_or_default();
+            // Parse into the Struct
+            let profile_struct = CfhdbPciProfile {
+                codename,
+                i18n_desc: "".to_string(),
+                icon_name,
+                class_ids,
+                vendor_ids,
+                device_ids,
+                blacklisted_class_ids,
+                blacklisted_vendor_ids,
+                blacklisted_device_ids,
+                packages,
+                install_script,
+                remove_script,
+                experimental,
+                removable,
+                priority: priority as i32,
+            };
+            dbg!(profile_struct);
             //driver_package_array.push(found_driver_package)
 
         }
     }
 }
 
-fn main_pci() {
-    let devices = CfhdbPciDevice::get_devices().unwrap();
-    for i in &devices {
+fn main() {
+    //let devices = CfhdbPciDevice::get_devices().unwrap();
+    /*for i in &devices {
         CfhdbPciDevice::set_available_profiles(&profiles, &i);
-    }
-    let hashmap = CfhdbPciDevice::create_class_hashmap(devices);
-    dbg!(hashmap);
+    }*/
+    //let hashmap = CfhdbPciDevice::create_class_hashmap(devices);
+    //dbg!(hashmap);
+    get_pci_profiles_from_url();
 }
 
 /*fn main() {
