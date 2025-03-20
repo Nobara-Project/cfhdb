fefd470b8e282a2053dc3974ca054cf26a0ba2da
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2025-03-21 00:18:33 +0300

    write sysfs helper

--- data/scripts/sysfs_helper.sh
+++ data/scripts/sysfs_helper.sh
@@ -3,13 +3,43 @@
 set -e
 
 blacklist_file_path="/etc/cfhdb/pci_blacklist"
+sysfs_remove_history="/tmp/cfhdb_sysfs_remove_history"
 
 start_device () {
-  echo "$2" | sudo tee /sys/bus/"$1"/drivers/"$3"/bind
+  if [ -f "$sysfs_remove_history" ]
+  then
+    DRIVER_NAME=$(grep "^$2 " "$sysfs_remove_history" | awk '{print $2}')
+    if [ -z "$DRIVER_NAME" ]; then
+      echo "No driver found for device $1."
+      exit 1
+    fi
+    echo "$2" > /sys/bus/"$1"/drivers/"$DRIVER_NAME"/bind
+    sed -i "/^$2 /d" "$sysfs_remove_history"
+  else
+    exit 1
+  fi
 }
 
 stop_device () {
-  echo "$2" | sudo tee /sys/bus/"$1"/devices/"$2"/driver/unbind
+  DRIVER_NAME=$(basename $(readlink "/sys/bus/"$1"/devices/"$2"/driver"))
+  if [ -z "$DRIVER_NAME" ]; then
+    echo "No driver found for device $1."
+    exit 1
+  fi
+  TEMP_TEXTLINE="$2 $DRIVER_NAME"
+  if [ -f "$sysfs_remove_history" ]
+  then
+    if grep -Fxq "$TEMP_TEXTLINE" "$sysfs_remove_history"
+    then
+      true
+    else
+      echo "$TEMP_TEXTLINE" >> "$sysfs_remove_history"
+    fi
+  else
+    touch "$sysfs_remove_history"
+    echo "$TEMP_TEXTLINE" >> "$sysfs_remove_history"
+  fi
+  echo "$2" > /sys/bus/"$1"/devices/"$2"/driver/unbind
 }
 
 enable_device () {
@@ -36,15 +66,15 @@ disable_device () {
 
 case "$1" in
     start_device)
-        start_device "$2" "$3" "$4"
+        start_device "$2" "$3"
         ;;
     stop_device)
-        stop_device "$2" "$3" "$4"
+        stop_device "$2" "$3"
         ;;
     enable_device)
-        enable_device "$2"
+        enable_device "$2" "$3"
         ;;
     disable_device)
-        disable_device "$2"
+        disable_device "$2" "$3"
         ;;
 esac
